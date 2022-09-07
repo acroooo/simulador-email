d16dd8e01698f26f48bac6a9ae87947d5c267927
Hernán Chamorro <hernang.work@gmail.com>
2022-09-06 21:14:48 -0300

    'initial+event_listeners'

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+// event listeners
+eventListeners();
+
+function eventListeners() {
+    console.log('eventListeners');
+    // cuando la app arranca
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+    // // validate the form
+    // document.querySelector('#enviar').addEventListener('click', validarFormulario);
+
+    // // reset the form
+    // document.querySelector('#resetBtn').addEventListener('click', resetForm);
+}
