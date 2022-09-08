d5cb93108444c1402ec5cb46752c97d443fedeba
Hernán Chamorro <hernang.work@gmail.com>
2022-09-08 13:54:28 -0300

    validar_email

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const btnEnviar = document.querySelector('#enviar')
 const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('#enviar-mail');
 
 // event listeners
 eventListeners();
@@ -26,12 +27,39 @@ function initApp(){
 }
 
 function validarFormulario(e) {
-    e.preventDefault();
 
+    // validar si hay texto
     if (e.target.value.length > 0) {
         console.log('Hay texto')
     } else {
-        e.target.style.color = 'white'
-        e.target.classList.add('opacity-50', 'border', 'border-red-500', 'border-solid',);
+        e.target.classList.add('border', 'border-red-500', 'border-solid',);
+        verError('Todos los campos son obligatorios');
+    }
+
+    // validar email
+    if (e.target.type === 'email') {
+        const resultado = e.target.value.indexOf('@');
+        if (resultado < 0) {
+            verError('Por favor, ingrese un email válido');
+        }
+
+    
     }
 }
+
+// error handling
+function verError(mensaje) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mb-5', 'text-center', 'error');
+
+    const errores = document.querySelectorAll('.error');
+
+    if (errores.length === 0) {
+        formulario.insertBefore(mensajeError, document.querySelector('.mb-10'));
+    }
+}
+
+// function campoValidado(campo) {
+//     campo.classList.add('border', 'border-green-500', 'border-solid',);
+// }
