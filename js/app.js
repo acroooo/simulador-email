0142e77ad9575a0376c53320829f422169164ffd
Hernán Chamorro <hernang.work@gmail.com>
2022-09-08 13:19:49 -0300

    validar_formulario_lengthofvalues

--- js/app.js
+++ js/app.js
@@ -12,16 +12,26 @@ eventListeners();
 function eventListeners() {
     console.log('eventListeners');
     // cuando la app arranca
-    document.addEventListener('DOMContentLoaded', iniciarApp);
-    // // validate the form
-    // document.querySelector('#enviar').addEventListener('click', validarFormulario);
+    document.addEventListener('DOMContentLoaded', initApp);
 
-    // // reset the form
-    // document.querySelector('#resetBtn').addEventListener('click', resetForm);
+    // campos del formulario -> validacion cuando se sale del campo
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
-
 // Funciones
 function initApp(){
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+function validarFormulario(e) {
+    e.preventDefault();
+
+    if (e.target.value.length > 0) {
+        console.log('Hay texto')
+    } else {
+        e.target.style.color = 'white'
+        e.target.classList.add('opacity-50', 'border', 'border-red-500', 'border-solid',);
+    }
 }
